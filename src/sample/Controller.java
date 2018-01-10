b4573905bb03cd90237ad57c9547e44fd51b66c6
Boutaleb Ali <ali.boutaleb.dev@outlook.fr>
2018-01-10 16:46:41 +0100

    add login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,69 @@
 package sample;
 
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controller {
 
-    public Label helloWorld;
+    // API
+    private static String API_URL = "http://localhost:8081";
 
-    public void sayHelloWorld(javafx.event.ActionEvent actionEvent) {
-        helloWorld.setText("Hellooooo");
+    // Login
+    public TextField email;
+    public PasswordField password;
+
+    // User
+    public String token = "";
+
+    public void login(javafx.event.ActionEvent actionEvent) {
+        try {
+            Map<String, String> params = new HashMap<>();
+            params.put("email",email.getText());
+            params.put("password",password.getText());
+            sendPostRequest("/auth/login", params);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void sendPostRequest(String path, Map<String, String> params) throws IOException{
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
+        // Config request
+        HttpPost request = new HttpPost(API_URL + path);
+        request.addHeader("Content-Type", "application/json");
+
+        // Set params
+        StringEntity parameters = new StringEntity(new JSONObject(params).toString());
+        request.setEntity(parameters);
+
+        // Execute request and get response
+        HttpResponse response = httpClient.execute(request);
+        int status = response.getStatusLine().getStatusCode();
+
+        // Set the token or send error message
+        if(status==200){
+            token = EntityUtils.toString(response.getEntity());
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Echec de l'authentification !");
+            alert.showAndWait();
+        }
+
+    }
+
 }
