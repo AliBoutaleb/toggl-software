6f9326a56173222ce00acbc9a017757d06c542bf
Boutaleb Ali <ali.boutaleb.dev@outlook.fr>
2018-01-11 15:55:21 +0100

    commit

--- src/modules/task/controller/TaskController.java
+++ src/modules/task/controller/TaskController.java
@@ -1,12 +1,18 @@
 package modules.task.controller;
 
-import org.json.JSONObject;
+import javafx.scene.control.ChoiceBox;
 import modules.common.Utils;
 import modules.common.model.ResponseObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
 public class TaskController {
+
+    public ChoiceBox projects;
 
     public void initialize(){
         listProject();
@@ -14,11 +20,13 @@ public class TaskController {
 
     public void listProject(){
         try {
-            ResponseObject res = Utils.sendGetRequest("/projects", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YTRiNTMxZDg3YzA0YzA4ZTQ5Yjc4MDciLCJpYXQiOjE1MTU2NzY1NTMsImV4cCI6MTUxNTY4MDE1M30._DFGf47UkwSZeaEK4ojp18tOvGZNk8GERcd715VFR-w");
-            System.out.println(res.getResponse());
-            JSONObject json = new JSONObject(res.getResponse().replace("\"","\""));
-            System.out.println(json);
+            ResponseObject res = Utils.sendGetRequest("/projects", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YTRiNTMxZDg3YzA0YzA4ZTQ5Yjc4MDciLCJpYXQiOjE1MTU2ODAyNDMsImV4cCI6MTUxNTY4Mzg0M30.LXZ25kaB0dibwTnPTq9ijEtKm2jSwQEdUmwyB5Y_meU");
+            JSONParser parser = new JSONParser();
+            JSONArray json = (JSONArray) parser.parse(res.getResponse());
+            projects.getItems().addAll(json);
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
